9ccb654d91448e7159ada1f38b555662c87276c6
Mestwin <mestwin@localhost.localdomain>
2019-06-05 11:10:05 +0200

    + Controller response refactor

--- src/main/java/net/mestwin/fcmpushnotifications/controller/PushNotificationController.java
+++ src/main/java/net/mestwin/fcmpushnotifications/controller/PushNotificationController.java
@@ -1,7 +1,9 @@
 package net.mestwin.fcmpushnotifications.controller;
 
 import net.mestwin.fcmpushnotifications.model.PushNotificationRequest;
+import net.mestwin.fcmpushnotifications.model.PushNotificationResponse;
 import net.mestwin.fcmpushnotifications.service.PushNotificationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,24 +22,24 @@ public class PushNotificationController {
     @PostMapping("/notification/topic")
     public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
         pushNotificationService.sendPushNotificationWithoutData(request);
-        return ResponseEntity.ok("A notification has been sent.");
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
     }
 
     @PostMapping("/notification/token")
     public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
         pushNotificationService.sendPushNotificationToToken(request);
-        return ResponseEntity.ok("A notification has been sent.");
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
     }
 
     @PostMapping("/notification/data")
     public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
         pushNotificationService.sendPushNotification(request);
-        return ResponseEntity.ok("A notification has been sent.");
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
     }
 
     @GetMapping("/notification")
     public ResponseEntity sendSampleNotification() {
         pushNotificationService.sendSamplePushNotification();
-        return ResponseEntity.ok("A notification has been sent.");
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
     }
 }
---
+++ src/main/java/net/mestwin/fcmpushnotifications/model/PushNotificationResponse.java
@@ -0,0 +1,31 @@
+package net.mestwin.fcmpushnotifications.model;
+
+public class PushNotificationResponse {
+
+    private int status;
+    private String message;
+
+    public PushNotificationResponse() {
+    }
+
+    public PushNotificationResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
